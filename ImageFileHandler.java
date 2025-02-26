24901db239876a4fbeb2ac1edee01e3fad4d3a43
BhumipatSaengduan <152416744+BhumipatSaengduan@users.noreply.github.com>
2025-02-26 15:51:48 +0700

    Update ImageFileHandler.java

--- ImageFileHandler.java
+++ ImageFileHandler.java
@@ -10,8 +10,8 @@ public class ImageFileHandler implements Handler {
     public void process(File file) {
         if (file.getFileType().equals("image")) System.out.println("Process image file by "+ handlerName);   
         else if (handler!=null) {
-        System.out.println(handlerName + " forwards request to " + handler.getHandlerName());
-        handler.process(file); 
+            System.out.println(handlerName + " forwards request to " + handler.getHandlerName());
+            handler.process(file); 
         } 
         else System.out.println("File not supported");
     }
